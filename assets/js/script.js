5882113c027a7a99a9e5876c97ed65167aed8492
brandiw <brandimichelle.0911@gmail.com>
2021-01-27 11:23:07 -0800

    Adding in better starter code hints

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log('Hi')
+// TODO: Declare any global variables we need
+
+
+// TODO: Fill in the blanks of this reusable function
+let updateScoreBoard = () => {
+    // TODO: Calculate the total number of rolls/flips
+    let total
+
+    // Make variables to track the percentages of heads and tails
+    let percentHeads = 0
+    let percentTails = 0
+
+    // TODO: Use the calculated total to calculate the percentages
+    // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
+
+    // TODO: Update the display of each table cell
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // This is just a sanity check to make sure your JavaScript script is getting loaded
+    // You can remove it once you see it in your browser console in the developer tools
+    console.log('Hi')
+
+    // TODO: Add event listener and handler for flip and clear buttons
+})
