8ebb7be2b611e9dae0096608e72d13da680e9745
brandiw <brandimichelle.0911@gmail.com>
2021-01-28 13:40:18 -0800

    Simplify code, students haven't seen functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
 // TODO: Declare any global variables we need
 
 
-// TODO: Fill in the blanks of this reusable function
-let updateScoreBoard = () => {
-    // TODO: Calculate the total number of rolls/flips
-    let total
-
-    // Make variables to track the percentages of heads and tails
-    let percentHeads = 0
-    let percentTails = 0
-
-    // TODO: Use the calculated total to calculate the percentages
-    // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-
-    // TODO: Update the display of each table cell
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
     console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+
+    // Flip Button Click Handler
+        // TODO: Determine flip outcome
+        // TODO: Update image and status message in the DOM
+
+        // Update the scorboard
+        // TODO: Calculate the total number of rolls/flips
+        // Make variables to track the percentages of heads and tails
+        // TODO: Use the calculated total to calculate the percentages
+        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
+        // TODO: Update the display of each table cell
+
+
+    // Clear Button Click Handler
+        // TODO: Reset global variables to 0
+        // TODO: Update the scoreboard (same logic as in flip button click handler)
+
 })
