6b02b2918f164dfb6f63a266b9b497828f0fe9f0
Tzvetomir Kotzev <tzvetomirkotzev@Tzvetomirs-MacBook-Air.local>
2022-11-21 22:06:20 -0500

    game finsihed penny flipping good

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 let numHeads = 0;
 let numTails = 0;
 let flipBut = document.querySelector("#flip");
-let claerBut = document.querySelector("#clear");
+let clearBut = document.querySelector("#clear");
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -15,37 +15,44 @@ document.addEventListener('DOMContentLoaded', function () {
     flipBut.addEventListener("click", (e) => {
         e.target.value = Math.floor(Math.random() * 2)
         //console.log(e.target.value)
+            // Flip Button Click Handler
+                // DONE: Determine flip outcome
+                // DONE: Update image and status message in the DOM
+                      // Update the scorboard
+                          // DONE: Calculate the total number of rolls/flips
+                          // Make variables to track the percentages of heads and tails
+                         // DONE: Use the calculated total to calculate the percentages
+                         // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
+                         // DONE: Update the display of each table cell
         if (e.target.value == true) {
             numHeads += 1
             document.querySelector("img").src = "./assets/images/penny-heads.jpg"
-            document.querySelector("#message") = "You flipped Heads!";
-            document.querySelector("#heads") = numHeads;
-            document.querySelector("#heads") = (100 * numHeads/(numHeads+numTails))+ '%';
+            document.querySelector("#message").textContent = "You flipped Heads!";
+            document.querySelector("#heads").textContent = numHeads;
+            document.querySelector("#heads-percent").textContent = (100 * numHeads/(numHeads+numTails)).toFixed(2)+ '%';
+            document.querySelector("#tails-percent").textContent = (100 * numTails/(numHeads+numTails)).toFixed(2)+ '%';
         } else {
             numTails += 1
             document.querySelector("img").src = "./assets/images/penny-tails.jpg"
-            document.querySelector("#message") = "You flipped Tails!";
-        }
-        //console.log(numHeads)
-        //console.log(numTails)
-    
+            document.querySelector("#message").textContent = "You flipped Tails!";
+            document.querySelector("#tails").textContent = numTails;
+            document.querySelector("#tails-percent").textContent = (100 * numTails/(numHeads+numTails)).toFixed(2) + '%';
+            document.querySelector("#heads-percent").textContent = (100 * numHeads/(numHeads+numTails)).toFixed(2) + '%';
+        } 
     })
 
-
-    // Flip Button Click Handler
-        // DONE: Determine flip outcome
-        // DONE: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
 
 
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+    clearBut.addEventListener("click", (e) => {
+        numTails = 0;
+        numHeads = 0;
+        document.querySelector("#message").textContent = "Let's Get Flipping!";
+        document.querySelector("#heads").textContent = numHeads;
+        document.querySelector("#tails").textContent = numTails;
+        document.querySelector("#heads-percent").textContent = 0+ '%';
+        document.querySelector("#tails-percent").textContent = 0+ '%';
+    })
 })
