1ce731917f78b6c384c3852c26fc3d66805690c4
Tzvetomir Kotzev <tzvetomirkotzev@Tzvetomirs-MacBook-Air.local>
2022-11-21 20:23:28 -0500

    almost done with coin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,9 @@
 // TODO: Declare any global variables we need
 //declaring Header, Flip and Clear button variables
-let headerBig = document.querySelctor("H1");
-let coinImg = document.createElement("img");
-let flipBut = document.createElement("button");
-let clearBut = document.createElement("button");
-let statusMes = document.createElement("h2");
-
-//section to set attributes to the Image, Clearn and Flip button and the Status objects
-
-
-//section to append the Image, buttons and status objects to teh header
-headerBig.append("coinImg");
-headerBig.append("flipBut");
-headerBig.append("clearBut");
-headerBig.append("statusMes");
+let numHeads = 0;
+let numTails = 0;
+let flipBut = document.querySelector("#flip");
+let claerBut = document.querySelector("#clear");
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -22,10 +12,26 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+    flipBut.addEventListener("click", (e) => {
+        e.target.value = Math.floor(Math.random() * 2)
+        //console.log(e.target.value)
+        if (e.target.value == true) {
+            numHeads += 1
+            document.querySelector("img").src = "./assets/images/penny-heads.jpg"
+            document.querySelector()
+        } else {
+            numTails += 1
+            document.querySelector("img").src = "./assets/images/penny-tails.jpg"
+        }
+        //console.log(numHeads)
+        //console.log(numTails)
+    
+    })
+
 
     // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+        // DONE: Determine flip outcome
+        // DONE: Update image and status message in the DOM
 
         // Update the scorboard
         // TODO: Calculate the total number of rolls/flips
