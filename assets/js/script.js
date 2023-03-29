51393525fe69c3751332302d4a5c22aaef8f78f8
Caleb Mieszko <caleb.mieszko@gmail.com>
2023-03-28 21:42:33 -0700

    Revert "Completed the coin flipper activity."
    This reverts commit c6e67fe1314342b9bfabb8e8e20831dc04b36127.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,27 @@
-let headsFlips = 0
-let tailsFlips = 0
-let headsPercent = '0%'
-let tailsPercent = '0%'
-let totalFlips = 0
+// TODO: Declare any global variables we need
 
 
 document.addEventListener('DOMContentLoaded', function () {
+    // This is just a sanity check to make sure your JavaScript script is getting loaded
+    // You can remove it once you see it in your browser console in the developer tools
+    console.log('Hi')
+
+    // TODO: Add event listener and handler for flip and clear buttons
 
     // Flip Button Click Handler
-    document.querySelector('#flip').addEventListener('click', function() {
-        let coinFlip = Math.ceil((Math.random() * 2));
-        if (coinFlip % 2 == 0) {
-            // flip heads
-            headsFlips++
-            totalFlips++
-            document.querySelector('#penny-image').src = './assets/images/penny-heads.jpg'
-            document.querySelector('#message').textContent = 'You Flipped Heads!'
-            document.querySelector('#heads').textContent = headsFlips
-            if (tails == 0){
-                return false
-            } else {
-                document.querySelector('#heads-percent').textContent = Math.round(headsFlips/totalFlips * 100) + '%'
-                document.querySelector('#tails-percent').textContent = Math.round(tailsFlips/totalFlips * 100) + '%'
-            }
-            
-        } else {
-            // flip tails
-            tailsFlips++
-            totalFlips++
-            document.querySelector('#penny-image').src = './assets/images/penny-tails.jpg'
-            document.querySelector('#message').textContent = 'You Flipped Tails!'
-            document.querySelector('#tails').textContent = tailsFlips
-            if (heads == 0){
-                return false
-            } else {
-                document.querySelector('#heads-percent').textContent = Math.round(headsFlips/totalFlips * 100) + '%'
-                document.querySelector('#tails-percent').textContent = Math.round(tailsFlips/totalFlips * 100) + '%'
-            }
-        }
-    })
+        // TODO: Determine flip outcome
+        // TODO: Update image and status message in the DOM
+
+        // Update the scorboard
+        // TODO: Calculate the total number of rolls/flips
+        // Make variables to track the percentages of heads and tails
+        // TODO: Use the calculated total to calculate the percentages
+        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
+        // TODO: Update the display of each table cell
 
 
     // Clear Button Click Handler
-    document.querySelector('#clear').addEventListener('click', function() {
-        headsFlips = 0
-        tailsFlips = 0
-        headsPercent = '0%'
-        tailsPercent = '0%'
-        totalFlips = 0
-        document.querySelector('#penny-image').src = './assets/images/penny-heads.jpg'
-        document.querySelector('#message').textContent = "Let's Get Flipping!"
-        document.querySelector('#heads').textContent = headsFlips
-        document.querySelector('#tails').textContent = tailsFlips
-        document.querySelector('#heads-percent').textContent = headsPercent
-        document.querySelector('#tails-percent').textContent = tailsPercent
-    })
-
+        // TODO: Reset global variables to 0
+        // TODO: Update the scoreboard (same logic as in flip button click handler)
 
 })
